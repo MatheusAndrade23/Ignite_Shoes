062901fdc181d9482ade0402e3f8ce059e39e1c0
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-04-02 19:49:22 -0300

    Bare Workflow

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+
+import OneSignal from 'react-native-onesignal';
+
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
@@ -8,6 +11,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId("deab7f33-7872-406c-8f8d-9668d0c4b8c7");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
