0c75d370e1b5deb9c517f2921f1feb94be639004
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-04-07 09:37:18 -0300

    Delete Tag

--- src/notifications/notificationTags.ts
+++ src/notifications/notificationTags.ts
@@ -1,8 +1,14 @@
 import OneSignal from "react-native-onesignal";
 
 
-// Criar Tags para o usuário
+// Criar Tag para o usuário
 
 export const tagUserEmailCreate = (email: string) => {
     OneSignal.sendTag("user_email", email)
+}
+
+// Excluir Tag para o usuário
+
+export const tagUserEmailDelete = (email: string) => {
+    OneSignal.deleteTag("user_email")
 }
